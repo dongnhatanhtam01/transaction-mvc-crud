4290af5880fe1344a908745e913a6c659463651c
dongnhatanhtam01 <dongnhatanhtam01@gmail.com>
2022-07-07 05:56:00 +0700

    first push

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+        }
+            
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
